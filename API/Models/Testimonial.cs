4ba3787043333e3175c06f3fad720edae7bbf89c
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-07-14 09:10:28 -0300

    Criado modelo Testimonial

---
+++ API/Models/Testimonial.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class Testimonial
+{
+    [Key]
+    [Required]
+    public int Id { get; set;}
+
+    [Required(ErrorMessage = "Informe a descrição")]
+    [MaxLength(80)]
+    public string Description { get; set;}
+
+    [Required(ErrorMessage = "Informe a foto")]
+    [MaxLength(500)]
+    public string Photo { get; set; }
+
+    [Required(ErrorMessage = "Informe seu nome")]
+    [MaxLength(80)]
+    public string PersonName { get; set; }
+}
