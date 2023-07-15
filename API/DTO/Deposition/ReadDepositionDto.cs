132f3de18271eb767824e33ccb477569811ddf24
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-07-15 17:56:48 -0300

    Adicionado Id no ReadDepositionDto

--- API/DTO/Deposition/ReadDepositionDto.cs
+++ API/DTO/Deposition/ReadDepositionDto.cs
@@ -4,6 +4,7 @@ namespace API.DTO.Deposition;
 
 public class ReadDepositionDto
 {
+    public int Id { get; set; }
     public string Description { get; set;}
 
     public string Photo { get; set; }
