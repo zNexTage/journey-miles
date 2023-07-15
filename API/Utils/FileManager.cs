9879dbd4c51c70da16cda32774c862788df464e9
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-07-15 14:36:37 -0300

    Armazenamento de arquivos no diretório da app.

---
+++ API/Utils/FileManager.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace API.Utils;
+public class FileManager
+{
+    private string BASE_PATH = "";
+
+    public FileManager(IWebHostEnvironment environment)
+    {
+        //Monta o camainho base onde será armazenados os arquivos
+        BASE_PATH = Path.Combine(environment.ContentRootPath, "media");
+
+        if(!Directory.Exists(BASE_PATH)){
+            Directory.CreateDirectory(BASE_PATH);
+        }
+    }
+
+    /// <summary>
+    /// Salva um arquivo enviado em uma requisição nos diretórios da aplicação
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <param name="file"></param>
+    /// <returns></returns>
+    public string SaveFile(string filename, IFormFile file)
+    {
+        var fullpath = Path.Combine(BASE_PATH, filename);
+
+        using (Stream fileStream = new FileStream(fullpath, FileMode.Create))
+        {
+            file.CopyTo(fileStream);
+        }
+
+        return fullpath;
+    }
+}
