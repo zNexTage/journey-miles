c1b4f67b7b998e3a96db7103590c053869f55f68
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-07-15 14:24:05 -0300

    Adicionado AutoMapper.

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,8 @@
+using API;
+using API.Service;
+using API.Utils;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>();
+
+// Injeção de dependência.
+builder.Services.AddScoped<DepositionService>();
+builder.Services.AddScoped<FileManager>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
