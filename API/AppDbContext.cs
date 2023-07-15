2cc526d61a950cdedd6daa1e1ed3d777d7cd44f4
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-07-15 14:22:55 -0300

    Configuração do DbContext.

---
+++ API/AppDbContext.cs
@@ -0,0 +1,28 @@
+using System;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Deposition> Depositions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = _configuration.GetConnectionString("Mysql");
+
+        optionsBuilder
+        .UseLazyLoadingProxies()
+        .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+        base.OnConfiguring(optionsBuilder);
+    }
+}
