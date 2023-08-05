a82ed816864bdab997278af4c8ff029b763989fa
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-08-05 18:51:01 -0300

    Created project Utils.
    Created a script to migrate the single photos of destinatin to the model
    Photos.

---
+++ Utils/MigratePhotoToPhotos.cs
@@ -0,0 +1,32 @@
+﻿using API;
+using Microsoft.Extensions.Configuration;
+
+namespace Utils;
+
+public class MigratePhotoToPhotos
+{
+    public static void Execute(IConfiguration configuration)
+    {
+        AppDbContext appDbContext = new(configuration);
+
+        var destinations = appDbContext.Destinations.ToList();
+
+        foreach (var dest in destinations)
+        {
+            // Photos photos = new () {
+            //     Destination = dest,
+            //     DestinationId = dest.Id,
+            //     Photo = dest.Photo
+            // };
+
+            // appDbContext.Photos.Add(photos);
+            // dest.Photo = "";
+            // appDbContext.SaveChanges();
+            // Console.WriteLine(dest.Photo);
+            foreach(var photo in dest.Photos){
+                Console.WriteLine(photo.Id);
+                Console.WriteLine(photo.Photo);
+            }
+        }
+    }
+}
---
+++ Utils/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using API;
+using API.Models;
+using Microsoft.Extensions.Configuration;
+using Utils;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
+
+MigratePhotoToPhotos.Execute(configuration);
+
